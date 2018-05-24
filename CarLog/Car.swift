faec8b9b181f1a17e36a057fee673831f60cd892
Hyuni89 <cuhcuh1@naver.com>
2018-05-25 00:17:27 +0900

    add CoreData example

--- CarLog/Car.swift
+++ CarLog/Car.swift
@@ -1,3 +1,4 @@
+import UIKit
 import Foundation
 import CoreData
 
@@ -13,6 +14,33 @@ class Car {
     }
     
     func load() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+
+        
+//        SAVE
+//        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+//        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+//        person.setValue(name, forKeyPath: "name")
+//
+//        do {
+//            try managedContext.save()
+//            people.append(person)
+//        } catch let error as NSError {
+//            print("Could not Save. \(error), \(error.userInfo)")
+//        }
+        
+//        LOAD
+//        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+//
+//        do {
+//            people = try managedContext.fetch(fetchRequest)
+//        } catch let error as NSError {
+//            print("Could not Fetch. \(error), \(error.userInfo)")
+//        }
     }
     
     func addData(data: Data) {
