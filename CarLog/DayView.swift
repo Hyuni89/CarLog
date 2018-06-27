f42a59854f89b9ac0c31d0c9385c3810dbd32368
Hyuni89 <cuhcuh1@naver.com>
2018-06-27 23:06:43 +0900

    update tableview on dayview

--- CarLog/DayView.swift
+++ CarLog/DayView.swift
@@ -1,33 +1,35 @@
 import Foundation
 import UIKit
 
-extension DayView: UITableViewDataSource {
+extension DayView: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "carListCell", for: indexPath) as! CarListCell
-        cell.carName.text = "Hello"
+        cell.carName.text = String(describing: (carData?[indexPath.row].mDistance)!)
+        cell.carInfo.text = String(describing: (carData?[indexPath.row].mFuel)!)
+        cell.defaultFlag.text = "-" + String(describing: (carData?[indexPath.row].mPrice)!)
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return (carData?.count)!
     }
 }
 
 class DayView: UIViewController {
     @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var costTableView: UITableView!
-    var car: Car?
+    var carData: [Data]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        costTableView.register(CarListCell.self, forCellReuseIdentifier: "carListCell")
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
+        carData = CarDataHandler.getInstance.getList(name: UserDefaults.standard.object(forKey: "DefaultCar") as! String)
+        costTableView.reloadData()
         self.calendar.setCurrentPage(Date(), animated: false)
     }
 }
